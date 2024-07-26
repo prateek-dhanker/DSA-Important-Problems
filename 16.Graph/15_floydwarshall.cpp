77431a9983f54059b6401c79675d1a10bee773d1
prateek-dhanker <prateekdhanker039@gmail.com>
2024-07-26 11:00:40 +0530

    FloydWarshall Added

---
+++ 16.Graph/15_floydwarshall.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> floydWarshall(int n,vector<vector<int>> &adjMatrix){
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(n,vector<int>(n,INT_MAX)));
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j)
+                    dp[0][i][j] = 0;
+                else if(adjMatrix[i][j])
+                    dp[0][i][j] = adjMatrix[i][j];
+            }
+        }
+
+        for(int k=1;k<=n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(dp[k-1][k-1][j]==INT_MAX || dp[k-1][i][k-1]== INT_MAX)
+                        dp[k][i][j] = dp[k-1][i][j];
+                    else
+                        dp[k][i][j] = min(dp[k-1][i][j] , dp[k-1][k-1][j]+dp[k-1][i][k-1]);
+                }
+            }
+        }
+
+        return dp[n];
+    }
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> adjMatrix(n,vector<int>(n,0));
+        for(auto &edge:edges){
+            int u=edge[0] , v=edge[1] ,w=edge[2];
+            adjMatrix[u][v] = adjMatrix[v][u] = w;
+        }
+        vector<vector<int>> minDist = floydWarshall(n,adjMatrix);
+
+        int mini = INT_MAX,minInd = 0;
+        for(int node=0;node<n;node++){
+            int cnt = 0;
+            for(int j=0;j<n;j++){
+                if(minDist[node][j] <= distanceThreshold)
+                    cnt++;
+            }
+            if(cnt<=mini){
+                mini = cnt;
+                minInd = node;
+            }
+        }
+        return minInd;
+    }
+};
