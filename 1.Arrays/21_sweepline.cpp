23df47afbbcf8ef7146a13ec0595dfab885a1595
prateek-dhanker <prateekdhanker039@gmail.com>
2025-01-05 14:38:13 +0530

    5Jan2025

---
+++ 1.Arrays/21_sweepline.cpp
@@ -0,0 +1,51 @@
+// You are given a string s of lowercase English letters and a 2D integer array shifts where shifts[i] = [starti, endi, directioni]. For every i, shift the characters in s from the index starti to the index endi (inclusive) forward if directioni = 1, or shift the characters backward if directioni = 0.
+
+// Shifting a character forward means replacing it with the next letter in the alphabet (wrapping around so that 'z' becomes 'a'). Similarly, shifting a character backward means replacing it with the previous letter in the alphabet (wrapping around so that 'a' becomes 'z').
+
+// Return the final string after all such shifts to s are applied.
+
+ 
+
+// Example 1:
+
+// Input: s = "abc", shifts = [[0,1,0],[1,2,1],[0,2,1]]
+// Output: "ace"
+// Explanation: Firstly, shift the characters from index 0 to index 1 backward. Now s = "zac".
+// Secondly, shift the characters from index 1 to index 2 forward. Now s = "zbd".
+// Finally, shift the characters from index 0 to index 2 forward. Now s = "ace".
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        int n = s.length();
+        vector<int> prefix(n+1,0);
+
+        for(auto shift:shifts){
+            if(shift[2]){
+                prefix[shift[0]]++;
+                prefix[shift[1]+1]--;
+            }
+            else{
+                prefix[shift[0]]--;
+                prefix[shift[1]+1]++;
+            }
+        }
+        for(int i=1;i<=n;i++)
+            prefix[i] += prefix[i-1];
+        
+        for(int i=0;i<n;i++){
+            int ch = s[i]-'a';
+
+            if(prefix[i]>=0)
+                ch = (ch+prefix[i])%26;
+            else
+                ch = (ch+26+(prefix[i]%26))%26;
+            
+            s[i] = 'a'+ch;
+        }
+        return s; 
+    }   
+};
