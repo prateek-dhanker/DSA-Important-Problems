0bccb4f284ceb494ea5f76449c4b79a2d5d226c7
prateek-dhanker <prateekdhanker039@gmail.com>
2025-01-24 21:52:47 +0530

    24Jan2025

--- 10.Tree/1_create_Traverse.cpp
+++ 10.Tree/1_create_Traverse.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Node{
@@ -51,6 +51,34 @@ void postOrderTraversal(Node *root){
         postOrderTraversal(root->right);
         cout<<root->data<<" ";
     }
+}
+vector<int> IterpostorderTraversal(Node* root) {
+    if(root == NULL)
+        return {};
+    vector<int> post;
+
+    stack<Node *> st1,st2;
+
+    st1.push(root);
+    while(st1.size()){
+        auto node = st1.top();
+        st1.pop();
+
+        st2.push(node);
+        
+        if(node->left)
+            st1.push(node->left);
+
+        if(node->right)
+            st1.push(node->right);            
+    }
+
+    while(st2.size()){
+        post.push_back(st2.top()->data);
+        st2.pop();
+    }
+
+    return post;
 }
 
 void levelOrderTraversal(Node * root){
