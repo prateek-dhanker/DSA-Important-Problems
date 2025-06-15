b0833544267fc77173e3135faeb641e84f1c1dc2
prateek-dhanker <prateek.engg7@gmail.com>
2025-06-15 12:31:04 +0530

    LRU cache doubly LL approach added

--- 7.LInkedList/11_LRUCache.cpp
+++ 7.LInkedList/11_LRUCache.cpp
@@ -45,3 +45,93 @@ public:
         size++;
     }
 };
+
+//Doubly-LL implementation
+class Node{
+public:
+    int key, val;
+    Node *prev, *next;
+
+    Node(int key, int val){
+        this->val = val;
+        this->key = key;
+        this->next = this->prev = NULL;
+    }
+};
+class LRUCache {
+public:
+    int capacity;
+    Node *head, *tail;
+    unordered_map<int,Node*> m;
+
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+
+        head = new Node(-1,-1);
+        tail = new Node(-1,-1);
+
+        head->next = tail;
+        tail->prev = head;
+    }
+    
+    int get(int key) {
+        if(m.find(key) == m.end())
+            return -1;
+        
+        Node* node = m[key];
+        //remove node
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+        //add after head
+        node->prev = head;
+        node->next = head->next;
+        head->next->prev = node;
+        head->next = node;
+
+        return node->val;
+    }
+    
+    void put(int key, int value) {
+        if(m.find(key) != m.end()){
+            Node* node = m[key];
+            //remove node
+            node->prev->next = node->next;
+            node->next->prev = node->prev;
+            //add after head
+            node->prev = head;
+            node->next = head->next;
+            head->next->prev = node;
+            head->next = node;
+
+            //update val and return
+            node->val = value;
+            return;
+        }
+        if(m.size() == capacity){
+            // remove the least recently used node
+            Node *delNode = tail->prev;
+
+            delNode->prev->next = tail;
+            tail->prev = delNode->prev;
+
+            m.erase(delNode->key);
+        }
+
+        Node* newNode = new Node(key,value);
+
+        //add after head
+        newNode->next = head->next;
+        newNode->prev = head;
+        head->next->prev = newNode;
+        head->next = newNode;
+
+        m[key] = newNode;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
